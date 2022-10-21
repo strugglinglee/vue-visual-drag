e3c9d94f48cd023c3cf1114698647667ec158dfe
strugglinglee <strugglinglee@163.com>
2022-10-21 19:52:40 +0800

    feat: init

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// 解决ts报错module cant find
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const vueComponent: DefineComponent<{}, {}, any>;
+  export default vueComponent;
+}
