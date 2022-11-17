3eb255ba7cf8c132e09daddf28776e8bdb8725ba
strugglinglee <strugglinglee@163.com>
2022-11-17 14:23:55 +0800

    feat: 改写calculate方法

--- src/utils/calculateComponentPositonAndSize.ts
+++ src/utils/calculateComponentPositonAndSize.ts
@@ -1,8 +1,5 @@
 import type { Point } from "@/types";
-// import { useMainStore } from "@/stores/main";
 import { getCenterPoint, angleToRadian } from "./translate";
-// import { drawTestPoints } from "./utils";
-
 /**
  * @method 计算根据圆心旋转后的点的坐标
  * @description 点a(x, y) 旋转中心c(x, y) 旋转后点n(x, y) 旋转角度θ
@@ -34,122 +31,136 @@ export const getTwoPointDistance = (point1: Point, point2: Point) => {
     Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
   );
 };
-
-function calculateLeftOrRight(
-  style: Record<string, any>,
-  curPositon: Point,
-  proportion: number,
-  needLockProportion: boolean,
-  pointInfo: Record<string, any>
-) {
-  const { symmetricPoint, curPoint } = pointInfo;
-  // 辅助点1：将鼠标指针以curPoint为圆点逆时针旋转rotate角度
-  const rotatedcurPositon = getPointByRotate(
-    curPositon,
-    curPoint,
-    -style.rotate
-  );
-  // 辅助点2: 横坐标和辅助点1一致，纵坐标和curPoint一致
-  const assistPoint2 = {
-    x: rotatedcurPositon.x,
-    y: curPoint.y,
-  };
-  // y不变
-  const rotatedLeftMiddlePoint = getPointByRotate(
-    assistPoint2,
-    curPoint,
-    style.rotate
-  );
-  const newWidth = getTwoPointDistance(rotatedLeftMiddlePoint, symmetricPoint);
-  const newCenter = getCenterPoint(rotatedLeftMiddlePoint, symmetricPoint);
-
-  let height = style.height;
-  if (needLockProportion) {
-    height = newWidth / proportion;
+class CalculateComponentStyle {
+  name: string;
+  style: Record<string, any>;
+  curPositon: Point;
+  proportion: number;
+  needLockProportion: boolean;
+  pointInfo: Record<string, any>;
+  constructor(
+    name: string,
+    style: Record<string, any>,
+    curPositon: Point,
+    proportion: number,
+    needLockProportion: boolean,
+    pointInfo: Record<string, any>
+  ) {
+    this.name = name;
+    this.style = style;
+    this.curPositon = curPositon;
+    this.proportion = proportion;
+    this.needLockProportion = needLockProportion;
+    this.pointInfo = pointInfo;
   }
+  calculateLeftOrRight() {
+    const { symmetricPoint, curPoint } = this.pointInfo;
+    // 辅助点1：将鼠标指针以curPoint为圆点逆时针旋转rotate角度
+    const rotatedcurPositon = getPointByRotate(
+      this.curPositon,
+      curPoint,
+      -this.style.rotate
+    );
+    // 辅助点2: 横坐标和辅助点1一致，纵坐标和curPoint一致
+    const assistPoint2 = {
+      x: rotatedcurPositon.x,
+      y: curPoint.y,
+    };
+    // y不变
+    const rotatedLeftMiddlePoint = getPointByRotate(
+      assistPoint2,
+      curPoint,
+      this.style.rotate
+    );
+    const newWidth = getTwoPointDistance(
+      rotatedLeftMiddlePoint,
+      symmetricPoint
+    );
+    const newCenter = getCenterPoint(rotatedLeftMiddlePoint, symmetricPoint);
 
-  style.height = height;
-  style.width = Math.round(newWidth);
-  style.top = Math.round(newCenter.y - style.height / 2);
-  style.left = Math.round(newCenter.x - newWidth / 2);
-}
-function calculateTopOrBootom(
-  style: Record<string, any>,
-  curPositon: Point,
-  proportion: number,
-  needLockProportion: boolean,
-  pointInfo: Record<string, any>
-) {
-  const { symmetricPoint, curPoint } = pointInfo;
-  // 辅助点1：将鼠标指针以curPoint为圆点逆时针旋转rotate角度
-  const rotatedcurPositon = getPointByRotate(
-    curPositon,
-    curPoint,
-    -style.rotate
-  );
-  // 辅助点2: 横坐标和curPoint一致 纵坐标和辅助点1一致
-  const assistPoint2 = {
-    x: curPoint.x,
-    y: rotatedcurPositon.y,
-  };
-  const rotatedTopMiddlePoint = getPointByRotate(
-    assistPoint2,
-    curPoint,
-    style.rotate
-  );
-  const newHeight = getTwoPointDistance(rotatedTopMiddlePoint, symmetricPoint);
-  const newCenter = getCenterPoint(rotatedTopMiddlePoint, symmetricPoint);
+    let height = this.style.height;
+    if (this.needLockProportion) {
+      height = newWidth / this.proportion;
+    }
 
-  let width = style.width;
-  if (needLockProportion) {
-    width = newHeight / proportion;
+    this.style.height = height;
+    this.style.width = Math.round(newWidth);
+    this.style.top = Math.round(newCenter.y - this.style.height / 2);
+    this.style.left = Math.round(newCenter.x - newWidth / 2);
   }
+  calculateTopOrBottom() {
+    const { symmetricPoint, curPoint } = this.pointInfo;
+    // 辅助点1：将鼠标指针以curPoint为圆点逆时针旋转rotate角度
+    const rotatedcurPositon = getPointByRotate(
+      this.curPositon,
+      curPoint,
+      -this.style.rotate
+    );
+    // 辅助点2: 横坐标和curPoint一致 纵坐标和辅助点1一致
+    const assistPoint2 = {
+      x: curPoint.x,
+      y: rotatedcurPositon.y,
+    };
+    const rotatedTopMiddlePoint = getPointByRotate(
+      assistPoint2,
+      curPoint,
+      this.style.rotate
+    );
+    const newHeight = getTwoPointDistance(
+      rotatedTopMiddlePoint,
+      symmetricPoint
+    );
+    const newCenter = getCenterPoint(rotatedTopMiddlePoint, symmetricPoint);
 
-  style.height = newHeight;
-  style.width = width;
-  style.top = Math.round(newCenter.y - newHeight / 2);
-  style.left = Math.round(newCenter.x - style.width / 2);
+    let width = this.style.width;
+    if (this.needLockProportion) {
+      width = newHeight / this.proportion;
+    }
+
+    this.style.height = newHeight;
+    this.style.width = width;
+    this.style.top = Math.round(newCenter.y - newHeight / 2);
+    this.style.left = Math.round(newCenter.x - this.style.width / 2);
+  }
+  calculateDoubleDirection() {
+    const { symmetricPoint } = this.pointInfo;
+    //求两点之间的中点坐标
+    const newCenterPoint = getCenterPoint(this.curPositon, symmetricPoint);
+    const newTopLeftPoint = getPointByRotate(
+      this.curPositon,
+      newCenterPoint,
+      -this.style.rotate
+    );
+    const newBottomRightPoint = getPointByRotate(
+      symmetricPoint,
+      newCenterPoint,
+      -this.style.rotate
+    );
+
+    const newWidth = Math.abs(newBottomRightPoint.x - newTopLeftPoint.x);
+    const newHeight = Math.abs(newBottomRightPoint.y - newTopLeftPoint.y);
+
+    this.style.width = Math.round(newWidth);
+    this.style.height = Math.round(newHeight);
+    this.style.left = Math.round(newCenterPoint.x - newWidth / 2);
+    this.style.top = Math.round(newCenterPoint.y - newHeight / 2);
+  }
+  calculate() {
+    if (["l", "r"].includes(this.name)) {
+      this.calculateLeftOrRight();
+      return;
+    }
+    if (["lt", "rt", "rb", "lb"].includes(this.name)) {
+      this.calculateDoubleDirection();
+      return;
+    }
+    if (["t", "b"].includes(this.name)) {
+      this.calculateTopOrBottom();
+      return;
+    }
+  }
 }
-function calculateDoubleDirection(
-  style: Record<string, any>,
-  curPositon: Point,
-  proportion: number,
-  needLockProportion: boolean,
-  pointInfo: Record<string, any>
-) {
-  const { symmetricPoint } = pointInfo;
-  //求两点之间的中点坐标
-  const newCenterPoint = getCenterPoint(curPositon, symmetricPoint);
-  const newTopLeftPoint = getPointByRotate(
-    curPositon,
-    newCenterPoint,
-    -style.rotate
-  );
-  const newBottomRightPoint = getPointByRotate(
-    symmetricPoint,
-    newCenterPoint,
-    -style.rotate
-  );
 
-  const newWidth = Math.abs(newBottomRightPoint.x - newTopLeftPoint.x);
-  const newHeight = Math.abs(newBottomRightPoint.y - newTopLeftPoint.y);
-
-  style.width = Math.round(newWidth);
-  style.height = Math.round(newHeight);
-  style.left = Math.round(newCenterPoint.x - newWidth / 2);
-  style.top = Math.round(newCenterPoint.y - newHeight / 2);
-}
-
-const funcs = {
-  l: calculateLeftOrRight,
-  lt: calculateDoubleDirection,
-  t: calculateTopOrBootom,
-  rt: calculateDoubleDirection,
-  r: calculateLeftOrRight,
-  rb: calculateDoubleDirection,
-  b: calculateTopOrBootom,
-  lb: calculateDoubleDirection,
-};
 /**
  * @method 计算组件坐标以及大小
  * @param name
@@ -162,17 +173,18 @@ const funcs = {
 export default function calculateComponentPositonAndSize(
   name: string,
   style: Record<string, any>,
-  curPositon: Record<string, any>,
+  curPositon: Point,
   proportion: number,
   needLockProportion: boolean,
   pointInfo: Record<string, any>
 ) {
-  (funcs as any)[name] &&
-    (funcs as any)[name](
-      style,
-      curPositon,
-      proportion,
-      needLockProportion,
-      pointInfo
-    );
+  const instance = new CalculateComponentStyle(
+    name,
+    style,
+    curPositon,
+    proportion,
+    needLockProportion,
+    pointInfo
+  );
+  instance.calculate();
 }
